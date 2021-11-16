54915e1857018e7ef2605229af9bcff5b15689c4
SmilinJasper <smilin.jasper@yahoo.com>
2021-11-16 23:36:23 +0530

    change-users-database-name

--- src/user.js
+++ src/user.js
@@ -4,17 +4,17 @@ import 'gun/axe';
 import { writable } from 'svelte/store';
 
 // Database
-export const db = GUN();
+export const database = GUN();
 
 // Gun User
-export const user = db.user().recall({sessionStorage: true});
+export const user = database.user().recall({ sessionStorage: true });
 
 // Current User's username
 export const username = writable('');
 
 user.get('alias').on(v => username.set(v))
 
-db.on('auth', async(event) => {
+database.on('auth', async(event) => {
     const alias = await user.get('alias'); // username string
     username.set(alias);
 
